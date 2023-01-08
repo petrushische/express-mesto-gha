a5e76a7216c60b6e963b86bc6b42b384a9e313bd
Пётр Румянцев <dogboxer64529000@gmail.com>
2023-01-09 00:10:43 +0300

    Создать модели

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 
-const { PORT = 3000 } = process.env;
+const mongoose = require('mongoose');
+
+const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(PORT);
-});
+async function connect() {
+  mongoose.set('strictQuery', false);
+  await mongoose.connect(MONGO_URL, {});
+  console.log(`Подключились к MongoDB ${MONGO_URL}`);
+  await app.listen(PORT);
+  console.log(`Успешное подключение, порт ${PORT}`);
+}
+
+connect();
