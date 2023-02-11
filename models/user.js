d8b055899a14fb4edbcb8d326d1072201263b5bb
Пётр Румянцев <dogboxer64529000@gmail.com>
2023-02-11 19:51:37 +0300

    hidden password

--- models/user.js
+++ models/user.js
@@ -37,12 +37,13 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    select: false,
   },
 });
 
 // eslint-disable-next-line func-names
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
