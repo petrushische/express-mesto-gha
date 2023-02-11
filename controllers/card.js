fe8d47b13ab411f8537ca36f7da96203662f3b8e
Пётр Румянцев <dogboxer64529000@gmail.com>
2023-02-11 19:28:20 +0300

    defender delete card

--- controllers/card.js
+++ controllers/card.js
@@ -8,6 +8,30 @@ module.exports.getCards = (req, res) => {
     .catch((err) => res.status(500).send({ message: 'На сервере произошла ошибка' }));
 };
 
+// eslint-disable-next-line no-unused-vars
+module.exports.cancellationDelete = (req, res, next) => {
+  cardSchema.findById(req.params.cardId)
+    .then((card) => {
+      if (!card) {
+        throw new Error('not found');
+      } else if (req.user._id !== card.owner._id.toHexString()) {
+        throw new Error('not Prava');
+      }
+      next();
+    })
+    .catch((err) => {
+      if (err.message === 'not found') {
+        res.status(404).send({ message: 'Такой карточки не существует' });
+      } else if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Неверный id, проверьте и введите еще раз' });
+      } else if (err.message === 'not Prava') {
+        res.send({ message: 'Вы не можете удалить эту карточку, так как не являетесь её создателем' });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
+    });
+};
+
 module.exports.getCardsId = (req, res) => {
   cardSchema.findByIdAndRemove(req.params.cardId)
     .populate(['owner'])
--- routes/cardRoutes.js
+++ routes/cardRoutes.js
@@ -3,10 +3,12 @@ const express = require('express');
 const router = require('express').Router();
 
 const {
-  createCard, getCardsId, getCards, likeCard, dislikeCard,
+  createCard, getCardsId, getCards, likeCard, dislikeCard, cancellationDelete,
 } = require('../controllers/card');
 
 router.get('/cards', getCards);
+
+router.delete('/cards/:cardId', cancellationDelete);
 
 router.delete('/cards/:cardId', getCardsId);
 
